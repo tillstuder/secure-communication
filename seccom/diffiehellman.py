0a2b70e4f88df9eada0748ac791693a575b5d2da
Till Studer <11232144+tillstud@users.noreply.github.com>
2020-09-20 19:33:05 +0200

    DiffieHellman v1.1

--- seccom/diffiehellman.py
+++ seccom/diffiehellman.py
@@ -1,8 +1,44 @@
-# Theory: https://www.youtube.com/watch?v=Yjrfm_oRO0w
+"""
+Use the Diffie-Hellman Service like this:
+
+dh = DiffieHellman(g, n)
+    n needs to be a large prime number
+    g needs to be primitive mod n
+
+With the Service you can:
+    generate private keys:
+    dh.gen_private_key()
+
+    generate public keys:
+    dh.gen_public_key(my_private_key)
+
+    generate the shared key:
+    dh.gen_key(my_private_key, peer_public_key)
+"""
 import random
 
 
-def theory():
+class DiffieHellman():
+    def __init__(self, g, n):
+        # "The number (n − l)/2 should also be a prime [1253]." Page 298 "Applied Cryptography" Bruce Schneier
+        self.n = n
+        self.g = g
+
+    def gen_private_key():
+        private_key = random.randint(5, 10)
+        return private_key
+
+    def gen_public_key(self, my_private_key):
+        public_key = (self.g**my_private_key) % self.n
+        return public_key
+
+    def gen_key(self, my_private_key, peer_public_key):
+        key = (int(peer_public_key)**int(my_private_key)) % self.n
+        return key
+
+
+def _theory():
+    # Theory: https://www.youtube.com/watch?v=Yjrfm_oRO0w
     g = random.randint(0, 9)
 
     # "The number (n − l)/2 should also be a prime [1253]." Page 298 "Applied Cryptography" Bruce Schneier
@@ -19,23 +55,3 @@ def theory():
 
     assert k_alice == k_bob
     print(k_alice == k_bob)
-
-
-class DiffieHellman():
-    def __init__(self, g, n):
-        # "The number (n − l)/2 should also be a prime [1253]." Page 298 "Applied Cryptography" Bruce Schneier
-        self.n = n
-        self.g = g
-
-    def gen_private_key(my_private_key):
-        private_key = random.randint(5, 10)
-        return private_key
-
-    def gen_public_key(self, my_private_key):
-        public_key = (self.g**my_private_key) % self.n
-        return public_key
-
-    def gen_key(self, my_private_key, peer_public_key):
-        key = (int(peer_public_key)**int(my_private_key)) % self.n
-        return key
-
