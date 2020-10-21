fba7a15c2b0e38dc126373e7c4fd42cd4ad53732
Till Studer <11232144+tillstud@users.noreply.github.com>
2020-10-21 09:00:28 +0200

    testing upstream

---
+++ seccom/des.py
@@ -0,0 +1,22 @@
+class DESCipher():
+    """
+    Use the DES Cipher Service like this:
+
+    des = DESCipher(message)
+        message needs to be a String
+
+    With the Service you can:
+        encrypt messages:
+        des.encrypt()
+
+        decrypt messages:
+        des.decrypt()
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def encrypt(self):
+        return result
+    
+    def decrypt(self):
+        return result
